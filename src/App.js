c81655b6522d6a9f210d6f120b18fefc8bb5a79f
Garrett Bogart <bogartgarrett@gmail.com>
2020-05-13 14:46:20 -0600

    added unit test and modified the app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="App-header">My Recipes</h1>
-      <p>There are no recipes to list.</p>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isAddRecipeFormDisplayed: false,
+  };
+
+  toggleAddRecipeForm = () => {
+    this.setState({
+      isAddRecipeFormDisplayed: !this.state.isAddRecipeFormDisplayed,
+    });
+  };
+
+  render() {
+    const addNewRecipeForm = <form id="recipe-form"></form>;
+
+    return (
+      <div className="App">
+        <h1 className="App-header">My Recipes</h1>
+        {this.state.isAddRecipeFormDisplayed ? (
+          addNewRecipeForm
+        ) : (
+          <button id="add-recipe" onClick={this.toggleAddRecipeForm}>
+            Add Recipe
+          </button>
+        )}
+        <p>There are no recipes to list.</p>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { shallow } from "enzyme";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("toggleAddRecipeForm() modifies isAddRecipeFormDisplayed state value to toggle visibility of a form on the page ", () => {
+  const wrapper = shallow(<App />);
+  wrapper.instance().toggleAddRecipeForm();
+
+  wrapper.update();
+  expect(wrapper.state().isAddRecipeFormDisplayed).toBeTruthy();
+  expect(wrapper.exists("#recipe-form")).toEqual(true);
+
+  wrapper.instance().toggleAddRecipeForm();
+  expect(wrapper.exists("recipe-form")).toEqual(false);
+  expect(wrapper.state().isAddRecipeFormDisplayed).toBeFalsy();
+});
+
+test("the Add Recipe button onClick calls the toggleAddRecipeForm method", () => {
+  const wrapper = shallow(<App />);
+  wrapper.instance().toggleAddRecipeForm = jest.fn();
+  wrapper.instance().forceUpdate();
+  // forceUpdate needs to be used because the wrapper instance that has already been rendered is not using the mock function,
+  // so React does not automatically detect that the method definition has been changed
+  const button = wrapper.find("#add-recipe");
+
+  button.simulate("click");
+
+  expect(wrapper.instance().toggleAddRecipeForm).toHaveBeenCalled();
 });
